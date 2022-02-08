732a47972208a2bade84214f79f28e612399aeeb
ayham-kattan <ayhamkattan1@gmail.com>
2022-02-08 17:40:51 +0200

    step2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Values from 'values.js';
 function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e)=> { 
+    e.preventDefault();
+    console.log('helloworld');
+  }
+
   return (
     <div className="App">
-      <h2>test</h2>
+      <h2>color Generator</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={color} onChange={e => { setColor(e.target.value) }} placeholder='#ff0' />
+        <button type="submit">Submit</button>
+        </form>
+      
     </div>
   );
 }
